6dd9074a618fc9737b09028662547ac40ec7b70d
gsbingo <gozeqiang@gmail.com>
2020-07-04 18:18:29 +0000

    initial commit

---
+++ src/main/java/com/test/bigtable/App.java
@@ -0,0 +1,55 @@
+package com.test.bigtable;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+import com.google.cloud.bigtable.hbase.BigtableConfiguration;
+import com.google.cloud.bigtable.hbase.BigtableOptionsFactory;
+import java.io.IOException;
+import org.apache.hadoop.hbase.client.Connection;
+import java.io.IOException;
+
+/*
+ * Hello world!
+ *
+ */
+public class App 
+{
+public static void main(String[] args) throws IOException {
+        String tableName = "test-bin";
+        SparkSession sc = SparkSession.builder().appName("test-bigtable").getOrCreate();
+        Configuration hbaseConf = BigtableConfiguration.configure("binguo-learning-centre", "binguo-test");
+
+        hbaseConf.set(TableInputFormat.INPUT_TABLE, tableName);
+        hbaseConf.set(TableInputFormat.SCAN_ROW_START, "104");
+        hbaseConf.set(TableInputFormat.SCAN_ROW_STOP,"105");
+
+        RDD<Tuple2<ImmutableBytesWritable, Result>> hBaseRDD = sc.sparkContext()
+                .newAPIHadoopRDD(hbaseConf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
+        
+	System.out.println("Number of Records found : " + hBaseRDD.count());
+        /**
+	JavaRDD<Tuple2<ImmutableBytesWritable, Result>> hBaseJavaRdd = hBaseRDD.toJavaRDD();
+        hBaseJavaRdd.foreach(v1 -> {
+            //获取行键
+            String key = new String(v1._2.getRow());
+            //通过列族和列名获取列
+            String name = new String(v1._2.getValue("cf1".getBytes(), "name".getBytes()));
+            String type = new String(v1._2.getValue("cf1".getBytes(), "type".getBytes()));
+            System.out.println("Row key:" + key + "\tcf1.Name:" + name + "\tcf1.type:" + type);
+        });
+        */
+
+        sc.stop();
+    }
+}
+
